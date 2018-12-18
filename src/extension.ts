49d857d58cc3537dec5d379ec14903ad11b32d2a
Johannes Rieken <johannes.rieken@gmail.com>
2018-12-18 11:41:40 +0100

    Revert "only reveal elements when the view is visible"
    This reverts commit 8cda0fcbcd3394296f4308e73fc8c6b17c6e9d10.

--- src/extension.ts
+++ src/extension.ts
@@ -167,10 +167,8 @@ export function activate(context: vscode.ExtensionContext) {
         const selection = view.selection[0] || model.first();
         const next = model.move(selection, fwd);
         if (next) {
+            view.reveal(next, { select: true });
             showRefCommand(next, true);
-            if (view.visible) {
-                view.reveal(next, { select: true });
-            }
         }
     };
 
